dcd7a8f91f91158b4a1a185886d007ff47bba7dd
amiransari27 <amir_ansari50@yahoo.in>
2023-12-23 19:29:29 +0530

    fibonacci

---
+++ fibonacci/index.ts
@@ -0,0 +1,13 @@
+function fibonacci(n: number): number[] {
+    const fib = [0,1]
+
+    for(let i=2; i< n; i++){
+        fib[i] = fib[i-1] + fib[i-2]
+    }
+    return fib
+}
+
+
+console.log(fibonacci(2))
+console.log(fibonacci(4))
+console.log(fibonacci(7))
