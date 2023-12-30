a29f80d8630120ed6a34fa2f1724ad8bcffc238d
amiransari27 <amir_ansari50@yahoo.in>
2023-12-30 12:20:03 +0530

    recursion

--- fibonacci/index.ts
+++ fibonacci/index.ts
@@ -1,3 +1,4 @@
+// time complexity O(n)
 function fibonacci(n: number): number[] {
     const fib = [0,1]
 
@@ -7,7 +8,47 @@ function fibonacci(n: number): number[] {
     return fib
 }
 
+// time complexity - O(n)
+// because at each level we are calling the function once
+function fibonacciRec (n: number): number[]{
+    if (n == 1){
+        return [0]
+    }
+    if (n == 2){
+        return [0,1]
+    }
+    const arr = fibonacciRec(n-1)
+    arr.push(arr[n-2] + arr[n-3])
+
+    return arr
+}
+
+// return nth element of fibonacci series
+// time complexity - O(2^n)
+// because at each level we are calling the function twice
+function fibonacciNthNumRec(n: number): number{
+
+    if (n === 0){
+        return 0
+    }else if (n === 1){
+        return 1
+    }
+
+    const num = fibonacciNthNumRec(n-1) + fibonacciNthNumRec(n-2)
+
+    return num
+}
+
 
 console.log(fibonacci(2))
 console.log(fibonacci(4))
 console.log(fibonacci(7))
+console.log("------")
+console.log(fibonacciRec(2))
+console.log(fibonacciRec(4))
+console.log(fibonacciRec(7))
+console.log("-----")
+console.log(fibonacciNthNumRec(2))
+console.log(fibonacciNthNumRec(4))
+console.log(fibonacciNthNumRec(7))
+
