acb49b209130f4fecc5621c91065f04a6d0f3e5b
amiransari27 <amir_ansari50@yahoo.in>
2024-01-04 15:00:27 +0530

    Create climbing.ts

---
+++ problems/climbing.ts
@@ -0,0 +1,14 @@
+{
+    function climbing(n: number): number{
+        const numOfWays = [1,2]
+
+        for(let i =2; i<n; i++){
+            numOfWays[i] = numOfWays[i-1] + numOfWays[i-2]
+        }
+        
+        return numOfWays[n-1]
+    }
+
+
+    console.log(climbing(4))
+}
