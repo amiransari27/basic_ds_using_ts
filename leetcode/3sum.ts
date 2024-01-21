01242780ab3b34463f9f6b32b83d0a2f1d64e479
amiransari27 <amir_ansari50@yahoo.in>
2024-01-21 13:21:14 +0530

    Update 3sum.ts - optimized

--- leetcode/3sum.ts
+++ leetcode/3sum.ts
@@ -5,8 +5,8 @@ function threeSum(nums: number[]): number[][] {
     for (let i = 0; i< nums.length -2 ; i++){
         for (let j = i+1; j< nums.length -1; j++){
             for (let k = j+1; k< nums.length; k++){
-                const tmp = [nums[i], nums[j], nums[5]]
-                tmp.sort((a,b)=>a-b)
+                let tmp = [nums[i], nums[j], nums[k]]
+                tmp = quickSort_3Sum(tmp)
 
                 const key = tmp.join("")
                 if (!memo[key]){
@@ -23,7 +23,83 @@ function threeSum(nums: number[]): number[][] {
     return arr
 };
 
+function threeSumOptimized(nums: number[]): number[][] {
+    if (nums.length === 0){
+        return []
+    }
+    const arr: number[][] = []
+    const memo: {[key: string]: boolean} = {}
 
-// console.log(threeSum([-1,0,1,2,-1,-4]))
-// console.log(threeSum([0,1,1]))
-console.log(threeSum([-2,0,1,1,2]))
+    nums = quickSort_3Sum(nums)
+
+    for (let i=0; i< nums.length - 2; i++){
+
+        let low = i+1
+        let high = nums.length - 1
+
+        while (low < high){
+            if (nums[i] + nums[low] + nums[high] === 0){
+                let tmp = [nums[i], nums[low], nums[high]]
+                const key = tmp.join("")
+                if (!memo[key]){
+                    memo[key] = true
+                    arr.push([nums[i], nums[low], nums[high]])
+                    
+                }
+                low++
+                high--
+            }else if (nums[i] + nums[low] + nums[high] < 0){
+                low++
+            }else {
+                high--
+            }
+        }
+
+
+    }
+
+
+    return arr
+};
+
+function quickSort_3Sum(nums:number[]): number[] {
+    
+    function sort(nums: number[], low: number, high: number){
+
+        if (low < high){
+            const pi = partition(nums, low, high)
+
+            sort(nums, low, pi - 1)
+            sort(nums, pi + 1, high)
+        }
+
+    }
+
+    function partition(nums: number[], low: number, high: number): number {
+        const pivot = nums[high]
+        let position = low - 1
+        
+        for (let i = low; i< high; i++){
+            if (nums[i] < pivot){
+                position++
+                const tmp = nums[position]
+                nums[position] = nums[i]
+                nums[i] = tmp
+            }
+        }
+        position++
+        const tmp = nums[position]
+        nums[position] = nums[high]
+        nums[high] = tmp
+        return position
+    }
+    
+    sort(nums, 0, nums.length - 1)
+
+    return nums
+}
+
+console.log(threeSumOptimized([-1,0,1,2,-1,-4]))
+// console.log(threeSumOptimized([0,1,1]))
+// console.log(threeSumOptimized([0,0,0,]))
+// console.log(threeSumOptimized([-2,0,1,1,2]))
