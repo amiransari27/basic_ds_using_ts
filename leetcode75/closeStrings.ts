ebc1e958e07a74e20df7c6742306941d954b1234
Amir Ansari <amir.amiransari987@gmail.com>
2024-02-06 12:03:47 +0530

    Update closeStrings.ts

--- leetcode75/closeStrings.ts
+++ leetcode75/closeStrings.ts
@@ -1,26 +1,28 @@
-function closeStrings(word1, word2) {
+function closeStrings(word1: string, word2: string): boolean {
     if (word1.length !== word2.length) {
-        return false;
+        return false 
     }
-    var vec1 = new Array(26).fill(0);
-    var vec2 = new Array(26).fill(0);
-    for (var i = 0; i < word1.length; i++) {
-        var idx1 = word1[i].charCodeAt(0) - 'a'.charCodeAt(0);
-        vec1[idx1] += 1;
-        var idx2 = word2[i].charCodeAt(0) - 'a'.charCodeAt(0);
-        vec2[idx2] += 1;
-    }
-    console.log(vec1)
-    console.log(vec2)
-    for (var i = 0; i < 26; i++) {
-        if (vec1[i] !== 0 && vec2[i] !== 0)
-            continue;
-        if (vec1[i] === 0 && vec2[i] === 0)
-            continue;
-        return false;
+    const vec1 = new Array(26).fill(0)
+    const vec2 = new Array(26).fill(0)
+    for (let i=0;i< word1.length; i++){
+        let idx1 = word1[i].charCodeAt(0) - 'a'.charCodeAt(0)
+        vec1[idx1] += 1
+
+        let idx2 = word2[i].charCodeAt(0) - 'a'.charCodeAt(0)
+        vec2[idx2] += 1
     }
 
-    return vec1.sort().toLocaleString() === vec2.sort().toLocaleString();
-}
-;
-console.log(closeStrings("abbzzca", "babzzcz"));
+    for (let i=0; i< 26; i++){
+        if (vec1[i] !== 0 && vec2[i] !== 0) continue
+        if (vec1[i] === 0 && vec2[i] === 0) continue
+
+        return false
+    }
+    return vec1.sort().toLocaleString() === vec2.sort().toLocaleString()
+    
+
+};
+
+console.log(
+    closeStrings("abc","bca")
+)
