a42f0fc52b87f507247845fb67375d3e4719a927
Amir Ansari <amir.amiransari987@gmail.com>
2024-02-13 11:45:13 +0530

    Create lowestCommonAncestor.ts

---
+++ leetcode75/lowestCommonAncestor.ts
@@ -0,0 +1,30 @@
+
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    if (root === null || root === p || root === q) {
+        return root
+    }
+
+    const ln = lowestCommonAncestor(root.left, p, q)
+    const rn = lowestCommonAncestor(root.right, p, q)
+
+    if (ln !== null && rn !== null) {
+        return root
+    }
+    if (ln !== null) {
+        return ln
+    }
+    return rn
+};
