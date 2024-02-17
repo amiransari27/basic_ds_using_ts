108ab66effa541d86149b16c54cee5596f298f14
Amir Ansari <amir.amiransari987@gmail.com>
2024-02-17 17:27:01 +0530

    Create guessNumber.ts

---
+++ leetcode75/guessNumber.ts
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+
+function guessNumber(n: number): number {
+   let low = 1
+   let high = n
+
+    function sol(low, high) {
+        if (low > high){
+            return -1
+        }
+        let mid = Math.floor((low + high)/2)
+        const res = guess(mid)
+        if (res === 0) {
+            return mid
+        } else if (res < 0) {
+            return sol(low, mid-1)
+        } else {
+            
+            return sol(mid+1, high)
+        }
+    }
+
+    return sol(low, high)
+};
