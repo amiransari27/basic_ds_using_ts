ca4d405dc680f74978e3d9248c2c2698778eadf2
Amir Ansari <amir.amiransari987@gmail.com>
2024-02-17 17:36:16 +0530

    Update guessNumber.ts

--- leetcode75/guessNumber.ts
+++ leetcode75/guessNumber.ts
@@ -7,26 +7,27 @@
  * var guess = function(num) {}
  */
 
+ function guess(num: number): number { return 0 }
 
-function guessNumber(n: number): number {
-   let low = 1
-   let high = n
-
-    function sol(low, high) {
-        if (low > high){
-            return -1
-        }
-        let mid = Math.floor((low + high)/2)
-        const res = guess(mid)
-        if (res === 0) {
-            return mid
-        } else if (res < 0) {
-            return sol(low, mid-1)
-        } else {
-            
-            return sol(mid+1, high)
-        }
-    }
-
-    return sol(low, high)
-};
+ function guessNumber(n: number): number {
+    let low = 1
+    let high = n
+ 
+     function sol(low, high) {
+         if (low > high){
+             return -1
+         }
+         let mid = Math.floor((low + high)/2)
+         const res = guess(mid)
+         if (res === 0) {
+             return mid
+         } else if (res < 0) {
+             return sol(low, mid-1)
+         } else {
+             
+             return sol(mid+1, high)
+         }
+     }
+ 
+     return sol(low, high)
+ };
